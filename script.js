b77f3e9eb43855e49efcab3364ae0a4978137f24
Dánae De la Cruz <danae_taiyaki@hotmail.com>
2026-09-14 15:55:42 -0600

    creacion del proyecto base

---
+++ script.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 900;
+canvas.height = 500;
+
+ctx.fillStyle = "#141414";
+ctx.fillRect(0,0, canvas.width, canvas.height);
